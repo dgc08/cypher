521543e186464dfeb7e4ccc079a810fab08ff247
Sinthoras39 <digc0820@gmail.com>
2024-05-02 14:13:21 +0100

    Started working on ESP32 1 # code
    Code is not tested + I don't know if it compiles, stuff like the
    threshold and time values should be rewieved, as they are example values

--- Laser/Laser.ino
+++ Laser/Laser.ino
@@ -1,5 +1,5 @@
-
 #include <WiFi.h>
+#include <esp_sleep.h>
 
 // WLAN zeug
 const char* ssid = "VSRO-Paed"; //ssid reinpacken
@@ -10,6 +10,7 @@ const int ldrPin = 33;
 
 
 const int threshold = 1400; //wie hell muss testen
+bool was_activated = false;
 
 void setup() {
   Serial.begin(115200); 
@@ -26,31 +27,56 @@ void setup() {
 }
 
 void loop() {
+  //TCP oder so
+  WiFiClient client;
+  const int serverPort = 6910; // kp welcher port
+  if (!(client.connect("10.1.252.159", serverPort))) { //ip
+    Serial.println("Verkackt mit Raspi zu reden");
+    return;
+  }
+  client.println("0");
+  while (client.available() == 0) {
+    delay(100);
+  }
+
+  char response = client.read();
+
+  switch (response) {
+    case '0':
+      esp_sleep_enable_timer_wakeup(10 * 1000000); // 10 seconds in microseconds
+      esp_deep_sleep_start();
+      return;
+    case '1':
+      break;
+    default:
+      Serial.print("Unknown response: ");
+      Serial.println(response);
+  }
+
   // Read LDR
   int ldrValue = analogRead(ldrPin);
 
   // Print the LDR value to the serial monitor
-  Serial.print("Laser Status: ");
-  Serial.println(ldrValue);
+  //Serial.print("Laser Status: ");
+  //Serial.println(ldrValue);
 
   // Tripwire an?
   if (ldrValue < threshold) {
     // Send a signal via WiFi
-    sendSignal();
+    if (!was_activated) {
+      sendSignal(client);
+      was_activated = true;
+    }
+  }
+  else {
+    was_activated = false
   }
 
+  client.stop();
   delay(1000); //kein spam pls
 }
 
-void sendSignal() {
-  //TCP oder so
-  WiFiClient client;
-  const int serverPort = 6910; // kp welcher port
-  if (client.connect("10.1.252.159", serverPort)) { //ip
-    // Send a simple message
-    client.println("Got you!");
-    client.stop();
-  } else {
-    Serial.println("Bruh kein wlan");
-  }
+void sendSignal(WiFiClient &client) {
+  // Send a simple message
+  client.println("1");
 }
