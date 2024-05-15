7506b404a949f7f82407eae06d6f019dab6b86c6
Sinthoras39 <digc0820@gmail.com>
2024-05-15 09:40:40 +0100

    omg all working (you can change state of laser now)

--- esp_connection.py
+++ esp_connection.py
@@ -13,6 +13,7 @@ class Laser(Monitor):
         self.BUFFER_SIZE = 1024
 
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
         self.server_socket.bind((HOST, PORT))
 
--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from esp_connection import Laser
 from log import Print_Logger, Flask_App
+from threading import Thread
 
 loggers = [Print_Logger(), Flask_App()]
 monitors = [Laser("laser_esp")]
 
+def cons():
+    a = input()
+    if a=="1" or a=="0":
+        if a=="0":
+            monitors[0].set_status(False)
+        if a=="1":
+            monitors[0].set_status(True)
+    else:
+        print("vergagt")
+    cons()
+
+# monitors[0].set_status(False)
+
 if __name__ == '__main__':
+    cons_thread = Thread(target=cons)
+    cons_thread.start()
+
     while True:
         events = []
         for i in monitors:
