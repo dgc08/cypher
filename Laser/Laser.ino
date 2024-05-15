34395d2e1445e550f2150471628531dac5ecde83
Sinthoras39 <digc0820@gmail.com>
2024-05-15 10:38:53 +0200

    omg all working now (esp32 2) (at least the communications threshold still trash)

--- Laser/Laser.ino
+++ Laser/Laser.ino
@@ -1,9 +1,56 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
 
+// WLAN zeug
+const char* ssid = SSID; //ssid reinpacken
+const char* password = WLAN_PASSWORD ; //wlan passwort reinpacken
+
+const int laserPin = 33; 
+
+void setup() {
+  Serial.begin(115200); 
+  pinMode(laserPin, OUTPUT);
+
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print("-");//funny
+  }
+  Serial.println("Connected to WiFi");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //TCP oder so
+  WiFiClient client;
+  const int serverPort = 6910; // kp welcher port
+  const char* ip = "10.1.253.67";
+  if (!(client.connect(ip, serverPort))) { //ip
+    Serial.println("Verkackt mit Raspi zu reden");
+    return;
+  }
+  Serial.println("New Iteration");
+  client.println("0");
+  while (client.available() == 0) {
+    delay(100);
+  }
 
+  char response = client.read();
+
+  Serial.println(response);
+
+  switch (response) {
+    case '0':
+      Serial.println("laser off");
+      digitalWrite(laserPin, LOW);
+      break;
+    case '1':
+      Serial.println("laser on");
+      digitalWrite(laserPin, HIGH);
+      break;
+    default:
+      Serial.print("Unknown response: ");
+      Serial.println(response);
+  }
+  delay(10 * 1000); // 10 Secs
 }
