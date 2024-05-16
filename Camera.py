29b3e05f7662c8d82804a5cc73647d1fc278d0a5
Sinthoras39 <digc0820@gmail.com>
2024-05-16 14:53:30 +0200

    files

--- Camera.py
+++ Camera.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 from classes import Monitor, Event
 from time import time
+from random import randint
 
 class Camera(Monitor):
     def _get_event(self):
-        return Event(time(), self._id, Event.events.COMPONENT_ACTIVATED, )
+        filename = "~/cypher/images/" + str(randint(1, 2147483647)) + ".jpg"
+
+
+
+        return Event(time(), self._id, Event.events.COMPONENT_ACTIVATED, filename)
