c368aaa98f0d670a9f2622c1485c9f69acefa5fb
Sinthoras39 <digc0820@gmail.com>
2024-05-16 14:53:30 +0200

    files

--- log.py
+++ log.py
@@ -1,10 +1,9 @@
 from classes import Logger
 import datetime
 import pytz
+from Discord_Bot import Discord_Bot
 
 class Print_Logger(Logger):
     def log(self, event):
         print (event.__str__())
 
-class Discord_Bot(Logger):
-    pass
