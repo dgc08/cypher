68ce885e0ec6f9e48dafa950b68780e88c344134
Sinthoras39 <digc0820@gmail.com>
2024-06-05 09:21:03 +0100

    CAmera

--- Camera.py
+++ Camera.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
 from classes import Monitor, Event
+
 from time import time
 import uuid
 from picamera2 import Picamera2, Preview
+from os import getenv
 
 camera = Picamera2()
-camera_config = camera.create_prewiev_configuration()
+camera_config = camera.create_still_configuration()
 camera.configure(camera_config)
 
 class Camera(Monitor):
     def _get_event(self):
         id = uuid.uuid4()
-        filename = "~/cypher/images/" + str(id) + ".jpg"
+        filename = getenv("HOME") + "/cypher/images/" + str(id) + ".jpg"
+
+        #camera.start_preview(Preview.QTGL)
+        camera.start()
+        camera.capture_file(filename)
 
         return Event(time(), self._id, Event.events.COMPONENT_ACTIVATED, filename)
+
+if __name__ == "__main__":
+    cam = Camera("mygog")
+    while True:
+        input("\nPress enter to take pic")
+        print(cam._get_event())
